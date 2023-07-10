a51fca3d1a0e31578ab08e7da10a9b01bef15172
Serhii Onopriienko <seriyrayne@gmail.com>
2023-07-10 19:33:38 +0200

    angularProject

---
+++ Angular project/todomvc/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, ItemComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ Angular project/todomvc/src/app/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {
+  @Input() allItems: any;
+  @Input() remaining: any;
+  @Input() filter: any;
+  @Input() completed: any;
+
+  @Output() changeFilterEvent = new EventEmitter();
+  @Output() removeCompletedEvent = new EventEmitter();
+
+  changeFilter(value) {
+    this.changeFilterEvent.emit(value);
+  }
+
+  removeCompleted() {
+    this.removeCompletedEvent.emit();
+  }
+}
