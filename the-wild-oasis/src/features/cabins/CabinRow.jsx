215d2f47e9bcfca514da190becf2069a6b3a9958
onopriienko.s <onopriienko.s@fsolutions.top>
2025-04-17 16:45:49 +0300

    new block finished

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -8,6 +8,7 @@ import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const Img = styled.img`
   display: block;
@@ -47,7 +48,7 @@ function CabinRow({ cabin }) {
     description,
   } = cabin;
 
-  const { isCreating, createCabin } = useCreateCabin();
+  const { createCabin } = useCreateCabin();
 
   const { isDeleting, deleteCabin } = useDeleCabin();
 
@@ -73,25 +74,31 @@ function CabinRow({ cabin }) {
         <span>&mdash;</span>
       )}
       <div>
-        <button disabled={isCreating} onClick={handleDuplicateCabin}>
-          <HiSquare2Stack />
-        </button>
-
         <Modal>
-          <Modal.Open opens={"edit"}>
-            <button>
-              <HiPencil />
-            </button>
-          </Modal.Open>
+          <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+
+            <Menus.List id={cabinId}>
+              <Menus.Button
+                icon={<HiSquare2Stack />}
+                onClick={handleDuplicateCabin}
+              >
+                Duplicate
+              </Menus.Button>
+
+              <Modal.Open opens={"edit"}>
+                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+              </Modal.Open>
+
+              <Modal.Open opens={"delete"}>
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+              </Modal.Open>
+            </Menus.List>
+          </Menus.Menu>
           <Modal.Window name={"edit"}>
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
-          <Modal.Open opens={"delete"}>
-            <button>
-              <HiTrash />
-            </button>
-          </Modal.Open>
           <Modal.Window name={"delete"}>
             <ConfirmDelete
               resourceName={"cabins"}
